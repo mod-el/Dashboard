a46086d9c25e25e129c82114cca1b185b78e5c21
Domenico Giambra <zorinik@gmail.com>
2018-06-08 20:03:12 +0200

    New customization options for "Total" card

--- Cards/Total.php
+++ Cards/Total.php
@@ -8,6 +8,8 @@ class Total extends Card
 	{
 		$options = array_merge([
 			'title' => null,
+			'text1' => null,
+			'text2' => null,
 			'admin-page' => null,
 			'table' => null,
 			'element' => null,
@@ -25,18 +27,29 @@ class Total extends Card
 
 		$tot = $this->model->_ORM->count($options['element'], $options['where'], [
 			'table' => $options['table'],
-		])
+		]);
+
+		if ($options['title']) {
+			?>
+			<div class="card-header text-center" style="font-size: 1.5rem"><?= entities($options['title']) ?></div>
+			<?php
+		}
 		?>
 		<div class="card-body text-center">
 			<?php
-			if (isset($options['title'])) {
+			if (isset($options['text1'])) {
 				?>
-				<h5 class="card-title"><?= entities($options['title']) ?></h5>
+				<h5 class="card-title"><?= entities($options['text1']) ?></h5>
 				<?php
 			}
 			?>
 			<h1 class="card-title"><?= $tot ?></h1>
 			<?php
+			if (isset($options['text2'])) {
+				?>
+				<h5 class="card-title"><?= entities($options['text2']) ?></h5>
+				<?php
+			}
 			if ($rule) {
 				?>
 				<a href="<?= $this->model->_AdminFront->getUrlPrefix() . $rule ?>" onclick="loadAdminPage(['<?= $rule ?>']); return false" class="card-link">Vai alla lista</a>
