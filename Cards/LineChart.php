5965ad33de4c003dd61f8c024089503849031546
Domenico Giambra <zorinik@gmail.com>
2018-06-10 15:01:42 +0200

    Moved charting generation from Dashboard to C3 module

--- Cards/LineChart.php
+++ Cards/LineChart.php
@@ -15,10 +15,6 @@ class LineChart extends Card
 			'having' => [],
 			'sum' => [],
 			'max' => [],
-			'fields' => [],
-			'label' => null,
-			'label-type' => null, // supported at the moment: timeseries
-			'values-type' => null, // supported at the moment: price
 		], $options);
 
 		$options = $this->getBasicOptions($options);
@@ -39,84 +35,15 @@ class LineChart extends Card
 			'max' => $options['max'],
 		]);
 
-		$chartColumns = [
-			['x'],
-		];
-		foreach ($options['fields'] as $idx => $f) {
-			$chartColumns[$idx + 1] = [
-				$this->getLabel($f),
-			];
-		}
-
-		foreach ($list as $elIdx => $el) {
-			$chartColumns[0][] = $options['label'] ? $el[$options['label']] : $elIdx;
-			foreach ($options['fields'] as $idx => $f)
-				$chartColumns[$idx + 1][] = $el[$f];
-		}
-
 		$this->renderTitle($options);
 		?>
 		<div class="card-body">
-			<div id="dashboard-chart-<?= $this->idx ?>"></div>
 			<?php
+			$options['id'] = 'dashboard-chart-' . $this->idx;
+			$this->model->_C3->lineChart($list, $options);
 			$this->renderListLink($options);
 			?>
 		</div>
-		<script>
-			<?php
-			$chartOptions = [
-				'bindto' => '#dashboard-chart-' . $this->idx,
-				'data' => [
-					'x' => 'x',
-					'columns' => $chartColumns,
-				],
-			];
-
-			switch ($options['label-type']) {
-				case 'timeseries':
-					if (!isset($options['label-format']))
-						$options['label-format'] = '%d/%m/%Y';
-
-					$chartOptions['axis'] = [
-						'x' => [
-							'type' => $options['label-type'],
-							'tick' => [
-								'format' => $options['label-format'],
-							],
-						],
-					];
-					break;
-			}
-			?>
-			var chartOptions = <?=json_encode($chartOptions, JSON_PRETTY_PRINT)?>;
-			<?php
-			switch ($options['values-type']) {
-			case 'price':
-			?>
-			chartOptions['tooltip'] = {
-				'format': {
-					'value': value => {
-						return makePrice(value);
-					},
-				}
-			};
-			<?php
-			break;
-			}
-			?>
-			var chart<?= $this->idx ?> = c3.generate(chartOptions);
-		</script>
 		<?php
-	}
-
-	/**
-	 * Converts a field name in a human-readable label
-	 *
-	 * @param string $k
-	 * @return string
-	 */
-	public function getLabel(string $k): string
-	{
-		return ucwords(str_replace(array('-', '_'), ' ', $k));
 	}
 }
