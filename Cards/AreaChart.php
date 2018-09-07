c7c60cc5d719549a6a8a6edb678ea876dde07369
Domenico Giambra <zorinik@gmail.com>
2018-09-07 22:21:02 +0200

    New "AreaChart" card type

---
+++ Cards/AreaChart.php
@@ -0,0 +1,44 @@
+<?php namespace Model\Dashboard\Cards;
+
+use Model\Dashboard\Card;
+
+class AreaChart extends Card
+{
+	public function render(array $options)
+	{
+		$options = array_merge([
+			'title' => null,
+			'data' => null,
+			'where' => [],
+			'limit' => null,
+			'order_by' => null,
+			'group_by' => false,
+			'having' => [],
+			'sum' => [],
+			'max' => [],
+
+			'chart-module' => 'Highcharts',
+			'fields' => [],
+			'label' => null,
+			'label-type' => null, // supported at the moment: datetime
+			'values-type' => null, // supported at the moment: price
+		], $options);
+
+		$options = $this->getBasicOptions($options);
+
+		$list = $this->model->_Dashboard->getListForCharting($options);
+
+		$this->renderTitle($options);
+
+		$chartModule = $this->model->getModule($options['chart-module']);
+		?>
+		<div class="card-body">
+			<?php
+			$options['id'] = 'dashboard-chart-' . $this->idx;
+			$chartModule->areaChart($list, $options);
+			$this->renderListLink($options);
+			?>
+		</div>
+		<?php
+	}
+}
